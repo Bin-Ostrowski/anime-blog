bbdecd2125af3d993292e1f882d5bff28a16d63c
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-09-09 10:15:42 -0700

    fixed bugs

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 //connect app to the browser's URL: import BrowserRouter, routes, route
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -6,9 +5,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigation from "./Components/Navigation";
 import Read from "./Components/Read";
-// import {useState} from "react";
+import {useState} from "react";
+import blogs from "./data";
+import NewBlog from './Components/NewBlog';
 
 function App() {
+  //set state at lowest level shared by both NewBlog and Read.
+  //inital state is blogs
+  const [blogState, setBlogState] = useState(blogs);
   return (
     //connect your app to the browser's URL: import
     //and render it around your whole app.
@@ -17,14 +21,16 @@ function App() {
       {/* navigation links and routes are in ./Components/Navigation */}
       {/* teach React Router how to render our app at different URLs by creating our first "Route Config" */}
       <Routes>
-        <Route path="/" element={<Read />} />
-        <Route path="/new_blog" element={<NewBlog />} />
+        {/* //pass in Read prop blogs=blogState state */}
+        <Route path="/" element={<Read blogs={blogState} />} />
+        {/* //pass into NewBlog a set blog state  */}
+        <Route
+          path="/new_blog"
+          element={<NewBlog setBlogState={setBlogState} />}
+        />
       </Routes>
     </BrowserRouter>
   );
 }
-
-const Home = () => <h1> You are home!!!!</h1>;
-const NewBlog = () => <h3> PLease write a new blog!!</h3>;
 
 export default App;
